2f96280304c7acc1690f13e316399f9ed2fabc20
EdwarMalpica <edwar.malpica@uptc.edu.co>
2023-08-17 10:33:22 -0500

    Auto-generated commit [ci skip]

--- src/app/data_managment/cv-investigators.ts
+++
@@ -1,8 +0,0 @@
-export class CvInvestigators {
-    Nombre!:string;
-    id_cv!:string
-    constructor(full_name:string, id:string){
-        this.Nombre = full_name
-        this.id_cv = id
-    }
-}
