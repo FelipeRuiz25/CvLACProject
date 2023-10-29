399695f572d5aab63cfc096b62144431ac4165d6
EstebanNovoa <esteban.novoa@uptc.edu.co>
2023-10-28 22:02:41 -0500

    upgrades investigator cv

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { SearchBarComponent } from 'src/app/components/search-bar/search-bar.component';
 import { CvInvestigators } from 'src/app/data_managment/cv-investigators';
 
@@ -12,7 +12,11 @@ export class HomeComponent implements OnInit{
   isLoaded: boolean = false;
   list_authors!:CvInvestigators[]
   
+  @Input() header_titles?: string[];
 
+  home_path = "home"
+  stats_path = "stats"
+  about_path = "about"
 
   getStateLoaded(data: any) {
     this.isLoaded = data; // Actualizar la propiedad del componente padre con el valor recibido
